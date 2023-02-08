c2d239ab96e66ebaefe51968c8a2af220f31b9e0
Michael Berry <michaelb@kentico.com>
2023-02-08 14:22:34 -0500

    Workaround for Netlify esLint build failure

--- src/IntegrationApp.tsx
+++ src/IntegrationApp.tsx
@@ -35,6 +35,7 @@ export const IntegrationApp: FC = () => {
         
       }
     });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [updateWatchedElementValue]);
 
   useEffect(() => {
