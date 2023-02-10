32adc6fa6f8d09bdd1a00c9fc78e194cb64ee0e7
Michael Berry <michaelb@kentico.com>
2023-02-10 08:58:33 -0500

    Adds lint ignore

--- src/IntegrationApp.tsx
+++ src/IntegrationApp.tsx
@@ -22,6 +22,7 @@ export const IntegrationApp: FC = () => {
   const showPreviouslySelectedValues = useCallback((codename:string) => {
     getExistingChecked(codename)
     setIsLoading(false)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
 
   useEffect(() => {
